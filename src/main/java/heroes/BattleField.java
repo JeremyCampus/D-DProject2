bbf36e0167d741777dc0a939e7d77c90c9480803
Jérémy Laurens <jeremy.laurens@Digital-Grenoble.local>
2018-03-21 10:31:39 +0100

    03 Java Heritage Terminé

--- src/main/java/heroes/BattleField.java
+++ src/main/java/heroes/BattleField.java
@@ -44,6 +44,7 @@ class BattleField {
 				System.out.println("Menu de gestion des personnages ? = 3");
 				System.out.println("Afficher un tableau de 5 objet ? = 4");
 				System.out.println("Quitter = 5");
+				System.out.println("Une division par 0 ? = 6");
 				System.out.println("---");
 				System.out.println("");
 				System.out.println("");
@@ -58,43 +59,33 @@ class BattleField {
 				switch (str2)
 				{
 					case "1":
-						Character myHero = null;
-						String vocation = "";
-						System.out.println("Guerrier(1) ou Mage(2) ?");
-						vocation = sc.nextLine();
-
-						String myName ="";
-						System.out.println("Quel est son nom ?");
-						myName = sc.nextLine();
-
-						String newImage ="X";
-
-						int newLife =1;
-						System.out.println("Quel est votre life ?");
-						newLife = sc.nextInt();
-
-						int newStrenght = 1;
-						System.out.println("Quel est votre Strenght ?");
-						newStrenght = sc.nextInt();	
-
-						if(vocation == "1"){
-							myHero = new Guerrier(myName, newImage, newLife, newStrenght);
-						}else{
-							myHero = new Mage(myName, newImage, newLife, newStrenght);
-						}
-						listeHeroes.add(myHero);
+						createCharacter();
 						break;        
 					case "2":
 						for(int i = 0; i < listeHeroes.size(); i++){
 							System.out.println("");
 							System.out.println("");
-							System.out.println("Voici le hero : " + listeHeroes.get(i).getName());
+							System.out.println("Voici le hero : " + listeHeroes.get(i).getName() + " | Numero : " + i);
 							listeHeroes.get(i).afficher();
+
 						}			  
 						break;        
 					case "3":
-						// myHero.attaquer();			  
-						break;        
+						try {
+							int numHero = 0;
+							System.out.println("Quel personnage souhaitez vous modifier ?");
+							for(int i = 0; i < listeHeroes.size(); i++){
+								System.out.println("-----------------------");
+								System.out.println("Voici le hero : " + listeHeroes.get(i).getName() + " | Numero : " + i);
+							}	
+							numHero = sc.nextInt();	
+							listeHeroes.get(numHero).afficher();
+							gestionDuHero(numHero);	
+						} catch (Exception e) {
+							System.out.println("erreur");
+						}      
+						break;  
+						
 					case "4":
 					creationALaChaine();
 						;
@@ -102,18 +93,88 @@ class BattleField {
 					case "5":
 						quitter = true;
 						break;   
+					case "6":
+						try {
+							System.out.println(1/0);
+						  } catch (ArithmeticException e) {
+							System.out.println("Division par zéro !");
+						  }
+					  break; 						  
 					default:
-						System.out.println("Erreur");
+						System.out.println("");
 						break;        
 				}
 			}while(quitter != true);
-		System.out.println("___________________Merci d'avoir joué (^_^)_______________________");
+		System.out.println("___________________Merci De Votre Visite (^_^)_______________________");
 
 
 
 	}
+//-------------------------Selectionner/modifier Un personnage
+public static void gestionDuHero(int numHero){
+	boolean exit = false;			  
+	Scanner sc = new Scanner(System.in);
+	clearZone();		  
+	do{
+		listeHeroes.get(numHero).mettreAJour();
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("_____________________________________________________________________");
+		System.out.println("|||||||||||||||||||||| QUE VOULEZ VOUS FAIRE ? ||||||||||||||||||||||");
+		System.out.println("_____________________________________________________________________");
+		System.out.println("---");
+		System.out.println("Afficher votre personnage ? = 1");
+		System.out.println("Modifier les infos de votre personnage? = 2");
+		System.out.println("Attaquer ? = 3");
+		System.out.println("Ajouter une arme ou un sort?  = 4");
+		System.out.println("Supprimer ce personnage ?  = 5");
+		System.out.println("Quitter  = 6");
+		System.out.println("---");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		String str2 = sc.nextLine();
+		clearZone();		  		
+		System.out.println("_____________________________________________________________________");
+		System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+		System.out.println("_____________________________________________________________________");
 
-	
+		switch (str2)
+		{
+			case "1":
+				listeHeroes.get(numHero).afficher();			  
+				break;        
+			case "2":
+				listeHeroes.get(numHero).modifier();			  
+				break;        
+			case "3":
+				listeHeroes.get(numHero).attaquer();			  
+				break;        
+			case "4":
+				listeHeroes.get(numHero).ajouterArmeSpell();			  
+				break;        
+			case "5":
+			System.out.println("Etes vous sur ?");
+			System.out.println("y(Yes), n(No)");
+			String validSuppr = sc.nextLine();
+			if(validSuppr == "y" || validSuppr == "Y")
+			{
+				listeHeroes.remove(numHero);
+				exit = true;
+			}
+				break; 
+			case "6":
+				exit = true;
+				break;        
+			default:
+		System.out.println("Erreur");
+				break;        
+		}
+	}while(exit != true);
+}
 
 //-------------------------AFFICHER TEMPORAIRE
 		public static void creationALaChaine(){
@@ -129,99 +190,41 @@ class BattleField {
 			tableauGuerrier[2]=myHero3;
 			tableauGuerrier[3]=myHero4;
 			tableauGuerrier[4]=myHero4;
-			System.out.println("Voici le hero : " + tableauGuerrier[0].getName());
-			System.out.println("Voici le hero : " + tableauGuerrier[1].getName());
-			System.out.println("Voici le hero : " + tableauGuerrier[2].getName());
-			System.out.println("Voici le hero : " + tableauGuerrier[3].getName());
-			System.out.println("Voici le hero : " + tableauGuerrier[4].getName());
-
+			for(int i = 0; i < 5; i++){
+			System.out.println("Voici le hero : " + tableauGuerrier[i].getName());
+			}
 		}
 
 
 //-------------------------CREATION PERSONNAGE OLD
 	public static void createCharacter()
 	{
-		Character heroes = new Character();
-		
-	// myHero.mettreAJour();
+		Scanner sc = new Scanner(System.in);
+		Character myHero = null;
 
-		boolean exitTotal = false;			  
-		do{
-	// 		System.out.println("-BATTLEFIELD-");
-	// 		System.out.println("Guerrier, ou Mage ?");
-	// 		System.out.println("Guerrier = 1 |  Mage = 2");
-	// 		Scanner sc = new Scanner(System.in);
-	// 		System.out.println("Veuillez saisir une valeur :");
-	// 		int str = sc.nextInt();
-	// 		Character myHero = null;
-			
-	// 		if(str!=1 && str!=2)
-	// 		{			
-	// 			System.out.println("Vous avez saisi : " + str + " C'est incorrecte");
-	// 		}else {			
-	// 			if(str==1)
-	// 			{			
-	// 				myHero = new Guerrier("Guerrier du Nord");
-	// 			}else if(str==2)
-	// 			{			
-	// 				myHero = new Mage("Mage de l'Ouest");
-	// 			} 
-				
-	// 			myHero.afficher();
-	// 			boolean exit = false;			  
+		String vocation = "";
+		System.out.println("Guerrier(1) ou Mage(2) ?");
+		vocation = sc.nextLine();
 
-	// 			do{
-	// 				myHero.mettreAJour();
-	// 				System.out.println("");
-	// 				System.out.println("");
-	// 				System.out.println("");
-	// 				System.out.println("_____________________________________________________________________");
-	// 				System.out.println("|||||||||||||||||||||| QUE VOULEZ VOUS FAIRE ? ||||||||||||||||||||||");
-	// 				System.out.println("_____________________________________________________________________");
-	// 				System.out.println("---");
-	// 				System.out.println("Afficher votre personnage ? = 1");
-	// 				System.out.println("Modifier les infos de votre personnage? = 2");
-	// 				System.out.println("Attaquer ? = 3");
-	// 				System.out.println("Ajouter une arme ou un sort?  = 4");
-	// 				System.out.println("Faire la saisie de plusieurs personnages  = 5");
-	// 				System.out.println("Quitter  = 6");
-	// 				System.out.println("---");
-	// 				System.out.println("");
-	// 				System.out.println("");
-	// 				System.out.println("");
-	// 				System.out.println("");
-	// 				System.out.println("");
-	// 				String str2 = sc.nextLine();
-	// 				System.out.println("_____________________________________________________________________");
-	// 				System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-	// 				System.out.println("_____________________________________________________________________");
-	// 				switch (str2)
-	// 				{
-	// 					case "1":
-	// 						myHero.afficher();			  
-	// 						break;        
-	// 					case "2":
-	// 						myHero.modifier();			  
-	// 						break;        
-	// 					case "3":
-	// 						myHero.attaquer();			  
-	// 						break;        
-	// 					case "4":
-	// 						myHero.ajouterArmeSpell();			  
-	// 						break;        
-	// 					case "5":
-	// 						exit = true;
-	// 						break; 
-	// 					case "6":
-	// 						exit = true;
-	// 						exitTotal = true;		  
-	// 						break;        
-	// 					default:
-	// 				System.out.println("Erreur");
-	// 						break;        
-	// 				}
-	// 			}while(exit != true);
-			// }
-		}while(exitTotal != true);
+		String myName ="";
+		System.out.println("Quel est son nom ?");
+		myName = sc.nextLine();
+
+		String newImage ="X";
+
+		int newLife =1;
+		System.out.println("Quel est votre life ?");
+		newLife = sc.nextInt();
+
+		int newStrenght = 1;
+		System.out.println("Quel est votre Strenght ?");
+		newStrenght = sc.nextInt();	
+
+		if(vocation == "1"){
+			myHero = new Guerrier(myName, newImage, newLife, newStrenght);
+		}else{
+			myHero = new Mage(myName, newImage, newLife, newStrenght);
+		}
+		listeHeroes.add(myHero);
 	}
 }
